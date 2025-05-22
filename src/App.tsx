22df672e892a4c7737367bdf3cc5665e3daa01ee
Thiago Cordeiro <thiagoty52@gmail.com>
2025-05-22 15:46:46 -0300

    feat: adds query client to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+import { useQuery, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
 export function App() {
-  return <h1>Ok</h1>;
+  const { data } = useQuery(
+    {
+      queryKey: ["users"],
+      queryFn: async (): Promise<IUser[]> => {
+        const response = await fetch("http://localhost:3000/users");
+        return response.json();
+      },
+    },
+    queryClient
+  );
+
+  return (
+    <div>
+      {data?.map((user) => (
+        <div key={user.id}>
+          <strong className="block">{user.name}</strong>
+          <small>{user.email}</small>
+        </div>
+      ))}
+    </div>
+  );
 }
